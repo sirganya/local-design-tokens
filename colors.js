72ca6441ad1abddf1a05ec8cf8070c486244b3c1
Greg Kavanagh <greg@concretenoise.com>
2020-11-17 13:15:18 +0000

    modularise

--- colors.js
+++
@@ -1,7 +0,0 @@
-const colors = {
-   'colorPrimary': 'rgba(229,131,0,1)',
-   'colorSecondary': 'rgba(255,154,20,1)',
-   'colorYellow300': 'rgba(255,174,66,1)',
-   'colorYellow200': 'rgba(255,194,112,1)',
-   'colorYellow100': 'rgba(255,213,158,0.49)',
-}
