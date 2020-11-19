f6ad039cec88d09a7786ae1a2230f0871c10203e
Greg Kavanagh <greg@concretenoise.com>
2020-11-19 15:58:26 +0000

    auto update

--- tokens/colors.js
+++ tokens/colors.js
@@ -3,5 +3,5 @@ export const colors = {
  colorSecondary: 'hsla(34,100%,53.9%,1.000)',
  colorYellow300: 'hsla(34,100%,62.9%,1.000)',
  colorYellow200: 'hsla(34,100%,72%,1.000)',
- colorYellow100: 'hsla(34,100%,81%,0.494)',
+ colorYellow100: 'hsla(236,85%,49.6%,0.494)',
 }
