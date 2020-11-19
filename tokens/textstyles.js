0317633a94a0cfc926939db4d3314a1787eedc2f
Greg Kavanagh <greg@concretenoise.com>
2020-11-19 14:43:08 +0000

    auto update

--- tokens/textstyles.js
+++ tokens/textstyles.js
@@ -1,33 +1,33 @@
 export const textStyles = {
-	font-h-1: {
+	fontH1: {
 		fontSize: "2rem",
 		fontWeight: 600,
 		lineHeight: "1.25",
 		letterSpacing: "-0.01em",
 		textTransform: "none",
 	},
-	font-h-2: {
+	fontH2: {
 		fontSize: "1.5rem",
 		fontWeight: 600,
 		lineHeight: "1.33",
 		letterSpacing: "-0.01em",
 		textTransform: "none",
 	},
-	font-h-3: {
+	fontH3: {
 		fontSize: "1rem",
 		fontWeight: 600,
 		lineHeight: "1.5",
 		letterSpacing: "-0.01em",
 		textTransform: "none",
 	},
-	font-body-light: {
+	fontBodyLight: {
 		fontSize: "0.75rem",
 		fontWeight: 300,
 		lineHeight: "1.33",
 		letterSpacing: "0em",
 		textTransform: "none",
 	},
-	font-body: {
+	fontBody: {
 		fontSize: "0.75rem",
 		fontWeight: 500,
 		lineHeight: "1.33",
